6daf0632d6fb054573fe6ec81b948bde5deff4b8
Kyu-u <melanchthonbutarbutar@gmail.com>
2022-02-22 18:11:58 +0700

    user page tests

--- src/__tests__/pages/Users.test.tsx
+++ src/__tests__/pages/Users.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Users from "../../pages/Users";
@@ -6,6 +6,11 @@ import { store } from "../../redux";
 import UserEvent from "@testing-library/user-event";
 
 describe("Users Page Testing", () => {
+  afterEach(() => {
+    jest.resetModules();
+    cleanup();
+  });
+
   it("onmount test", async () => {
     render(
       <Provider store={store}>
@@ -15,25 +20,40 @@ describe("Users Page Testing", () => {
       </Provider>
     );
 
-    expect(await screen.findByText('Leanne Graham')).toBeInTheDocument();
+    expect(await screen.findByText("Leanne Graham")).toBeInTheDocument();
   });
-  it("modal yes click test", async () => {
-    const page = render(
+  it("modal no click test", async () => {
+    render(
       <Provider store={store}>
         <BrowserRouter>
           <Users />
         </BrowserRouter>
       </Provider>
     );
-    const delButton = await screen.findByTestId('delete-button-1')
+    const delButton = await screen.findByTestId("delete-button-1");
     UserEvent.click(delButton);
-    expect(await screen.findByText('Delete User')).toBeInTheDocument();
+    expect(await screen.findByText("Delete User")).toBeInTheDocument();
 
-    const yesButton = await screen.findByTestId('yes-button');
+    const yesButton = await screen.findByTestId("no-button");
     UserEvent.click(yesButton);
-    expect(await screen.findByText('Leanne Graham')).not.toBeInTheDocument();
+    expect(await screen.findByText("Leanne Graham")).toBeInTheDocument();
 
     // expect(await screen.findByText('Leanne Graham')).toBeInTheDocument();
+  });
+  it("modal yes click test", async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Users />
+        </BrowserRouter>
+      </Provider>
+    );
+    const delButton = await screen.findByTestId("delete-button-1");
+    UserEvent.click(delButton);
+    expect(await screen.findByText("Delete User")).toBeInTheDocument();
 
-  })
+    const yesButton = await screen.findByTestId("yes-button");
+    UserEvent.click(yesButton);
+    expect(screen.queryByText("Leanne Graham")).not.toBeInTheDocument();
+  });
 });
